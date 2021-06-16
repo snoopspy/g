000339ab184cf99354f6698afc6df83206a27e54
gilgil <gilgil@gilgil.net>
2021-06-16 21:24:01 +0900

    Set snoopspy installed path(/data/data/net.gilgil.snoopspy/files)

--- src/net/capture/gpcappipe.cpp
+++ src/net/capture/gpcappipe.cpp
@@ -21,6 +21,8 @@ struct packet_hdr_t {
 // GPcapPipe
 // ----------------------------------------------------------------------------
 GPcapPipe::GPcapPipe(QObject* parent) : GCapture(parent) {
+	QString path = "/data/data/net.gilgil.snoopspy/files";
+	command_ = QString("adb exec-out su -c \"cd %1; export LD_LIBRARY_PATH=%2/../lib; ./corepcap dev wlan0 -f '' file -\"").arg(path, path);
 }
 
 GPcapPipe::~GPcapPipe() {
