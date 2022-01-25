9630e6b8ad59ff153defc014e26d38c13b3b6670
gilgil <gilgil@gilgil.net>
2022-01-25 22:44:15 +0900

    Fix some bugs

---
+++ src/net/process/gchannelhop.cpp
@@ -0,0 +1,76 @@
+#include "gchannelhop.h"
+
+#ifndef Q_OS_WIN
+
+// ----------------------------------------------------------------------------
+// GChannelHop
+// ----------------------------------------------------------------------------
+GChannelHop::GChannelHop(QObject* parent) : GStateObj(parent) {
+	GRtmEntry* entry = GNetInfo::instance().rtm().getBestEntry(QString("8.8.8.8"));
+	if (entry != nullptr) {
+		GIntf* intf = entry->intf();
+		if (intf != nullptr)
+			intfName_ = intf->name();
+	}
+}
+
+GChannelHop::~GChannelHop() {
+	close();
+}
+
+bool GChannelHop::doOpen() {
+	if (!iw_.open(qPrintable(intfName_))) {
+		SET_ERR(GErr::Fail, iw_.error_.data());
+		return false;
+	}
+	thread_.start();
+	return true;
+}
+
+bool GChannelHop::doClose() {
+	we_.wakeAll();
+	thread_.quit();
+	bool res = thread_.wait();
+	iw_.close();
+	return res;
+}
+
+void GChannelHop::run() {
+	qDebug() << "beg"; // gilgil temp 2022.01.24
+	QList<int> channelList;
+	for (QString c: channelList_) {
+		int channel = c.toInt();
+		if (channel != 0)
+			channelList.push_back(channel);
+	}
+	if (channelList.count() == 0) {
+		std::list<int> cl = iw_.channelList();
+		for (int c: cl)
+			channelList.push_back(c);
+	}
+	qDebug() << channelList; // gilgil temp 2022.01.24
+	while (active()) {
+		for (int channel: channelList) {
+			if (!iw_.setChannel(channel))
+				qDebug() << iw_.error_.data();
+			qDebug() << "current channel =" << channel; // gilgil temp 2022.01.24
+			emit channelChanged(channel);
+			if (we_.wait(hopInterval_)) break;
+		}
+	}
+	qDebug() << "end"; // gilgil temp 2022.01.24
+}
+
+#ifdef QT_GUI_LIB
+
+#include "base/prop/gpropitem-interface.h"
+GPropItem* GChannelHop::propCreateItem(GPropItemParam* param) {
+	if (QString(param->mpro_.name()) == "intfName") {
+		return new GPropItemInterface(param);
+	}
+	return GObj::propCreateItem(param);
+}
+
+#endif // QT_GUI_LIB
+
+#endif // Q_OS_WIN
---
+++ src/net/process/gchannelhop.h
@@ -0,0 +1,70 @@
+// ----------------------------------------------------------------------------
+//
+// G Library
+//
+// http://gilgil.net
+//
+// Copyright (c) Gilbert Lee All rights reserved
+//
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#ifndef Q_OS_WIN
+
+#include "base/gstateobj.h"
+#include "base/sys/gthread.h"
+#include "base/sys/gwaitevent.h"
+
+#include "net/gnetinfo.h"
+#include "net/iw/giw.h"
+
+// ----------------------------------------------------------------------------
+// GChannelHop
+// ----------------------------------------------------------------------------
+struct G_EXPORT GChannelHop : GStateObj {
+	Q_OBJECT
+	Q_PROPERTY(QString intfName MEMBER intfName_)
+	Q_PROPERTY(int hopInterval MEMBER hopInterval_)
+	Q_PROPERTY(QStringList channelList MEMBER channelList_)
+
+public:
+	QString intfName_{""};
+	int hopInterval_{1000};
+	QStringList channelList_;
+
+public:
+	Q_INVOKABLE GChannelHop(QObject* parent = nullptr);
+	~GChannelHop() override;
+
+protected:
+	GIw iw_;
+	GWaitEvent we_;
+
+protected:
+	bool doOpen() override;
+	bool doClose() override;
+
+protected:
+	struct Thread : GThread {
+		Thread(GChannelHop* channelHop) : GThread(channelHop) {}
+	protected:
+		void run() override {
+			GChannelHop* channelHop = dynamic_cast<GChannelHop*>(parent());
+			Q_ASSERT(channelHop != nullptr);
+			channelHop->run();
+		}
+	} thread_{this};
+
+	virtual void run();
+
+signals:
+	void channelChanged(int channel);
+
+#ifdef QT_GUI_LIB
+public:
+	GPropItem* propCreateItem(GPropItemParam* param) override;
+#endif // QT_GUI_LIB
+};
+
+#endif // Q_OS_WIN
