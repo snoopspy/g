d88cf2f74d2a02513acffb6b5b4756f3654e06b3
gilgil <gilgil@gilgil.net>
2022-02-05 23:02:17 +0900

    Change default settings

--- app/net/wa/wifianalyzer.cpp
+++ app/net/wa/wifianalyzer.cpp
@@ -4,9 +4,10 @@
 WifiAnalyzer::WifiAnalyzer(QObject* parent) : GStateObj(parent) {
 #ifdef Q_OS_ANDROID
 	command_.openCommands_.push_back(new GCommandItem(this, QStringList{"su -c \"nexutil -c1\"", "su -c \"nexutil -d\"", "su -c \"nexutil -m2\""}));
-	command_.closeCommands_.push_back(new GCommandItem(this, QStringList{"su -c \"nexutil -m0\""}));
+    command_.closeCommands_.push_back(new GCommandItem(this, QStringList{"su -c \"nexutil -m0\"", "su -c \"ifconfig wlan0 down\""}));
 #endif
 
+    dot11Block_.enabled_ = false;
 	dot11Block_.writer_ = &pcapDeviceWrite_;
 	pcapDeviceWrite_.mtu_ = 0;
 
