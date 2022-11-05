16f01e05f61173ba081f8ef28bf8c8e1a7bdce35
gilgil <gilgil@gilgil.net>
2022-11-05 16:53:31 +0900

    Change nexmon so file name

--- src/net/capture/gpcappipenexmon.cpp
+++ src/net/capture/gpcappipenexmon.cpp
@@ -5,7 +5,7 @@
 // ----------------------------------------------------------------------------
 GPcapPipeNexmon::GPcapPipeNexmon(QObject* parent) : GPcapPipe(parent) {
 	QString path = "/data/data/com.snoopspy/files";
-	command_ = QString("adb exec-out su -c \"export LD_LIBRARY_PATH=%1/../lib; export LD_PRELOAD=libfakeioctl.so; %2/corepcap dev wlan0 file -\"").arg(path, path);
+	command_ = QString("adb exec-out su -c \"export LD_LIBRARY_PATH=%1/../lib; export LD_PRELOAD=libnexmon.so; %2/corepcap dev wlan0 file -\"").arg(path, path);
 }
 
 GPcapPipeNexmon::~GPcapPipeNexmon() {
