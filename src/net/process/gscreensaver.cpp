48e539340202d61014fb975316845b5c35809282
gilgil <gilgil@gilgil.net>
2023-11-14 01:21:51 +0900

    Add GScreenSaver class

---
+++ src/net/process/gscreensaver.cpp
@@ -0,0 +1,96 @@
+#include "gscreensaver.h"
+
+#ifdef QT_GUI_LIB
+
+// ----------------------------------------------------------------------------
+// ScreenSaverWidget
+// ----------------------------------------------------------------------------
+ScreenSaverWidget::ScreenSaverWidget(GScreenSaver* screenSaver) : QWidget(nullptr), screenSaver_(screenSaver) {
+	QPalette palette(QPalette::Window, Qt::black);
+	setAutoFillBackground(true);
+	setPalette(palette);
+
+	label_ = new QLabel(this);
+	timer_ = new QTimer(this);
+
+	updateCurrentTime();
+
+	QObject::connect(timer_, &QTimer::timeout, this, &ScreenSaverWidget::updateCurrentTime);
+	timer_->start(screenSaver_->updateInterval_);
+}
+
+ScreenSaverWidget::~ScreenSaverWidget() {
+	qDebug() << "bef"; // gilgil temp 2023.11.14
+	if (screenSaver_->active())
+		screenSaver_->close();
+	qDebug() << "end"; // gilgil temp 2023.11.14
+}
+
+void ScreenSaverWidget::closeEvent(QCloseEvent *event) {
+	qDebug() << "beg"; // gilgil temp 2023.11.14
+	QWidget::closeEvent(event);
+	timer_->stop();
+	qDebug() << "end"; // gilgil temp 2023.11.14
+}
+
+void ScreenSaverWidget::mousePressEvent(QMouseEvent *event)  {
+	qDebug() << "beg"; // gilgil temp 2023.11.14
+	QWidget::mousePressEvent(event);
+	close();
+	qDebug() << "end"; // gilgil temp 2023.11.14
+}
+
+void ScreenSaverWidget::updateCurrentTime() {
+	QDateTime now = QDateTime::currentDateTime();
+	QString msg = now.toString(screenSaver_->format_);
+	label_->setText(msg);
+	label_->adjustSize();
+
+	QRect widgetRect = rect();
+	QRect labelRect = label_->rect();
+	qDebug() << widgetRect << labelRect;
+	int left = std::rand() % (widgetRect.width() - labelRect.width());
+	int top = std::rand() % (widgetRect.height() - labelRect.height());
+	label_->setGeometry(QRect(left, top, labelRect.width(), labelRect.height()));
+}
+
+// ----------------------------------------------------------------------------
+// GScreenSaver
+// ----------------------------------------------------------------------------
+bool GScreenSaver::doOpen() {
+	qDebug() << ""; // gilgil temp 2023.11.13
+	if (screenSaverWidget_ == nullptr) {
+		screenSaverWidget_ = new ScreenSaverWidget(this);
+	}
+
+	switch (showStyle_) {
+		case Show: screenSaverWidget_->show(); break;
+		case ShowMaxized: screenSaverWidget_->showMaximized(); break;
+		case ShowFullScreen: screenSaverWidget_->showFullScreen(); break;
+	}
+
+	return true;
+}
+
+bool GScreenSaver::doClose() {
+	qDebug() << "beg"; // gilgil temp 2023.11.13
+	if (screenSaverWidget_ != nullptr) {
+		delete screenSaverWidget_;
+		screenSaverWidget_ = nullptr;
+	}
+
+	qDebug() << "end"; // gilgil temp 2023.11.13
+	return true;
+}
+
+#else // QT_GUI_LIB
+bool GScreenSaver::doOpen() {
+	qWarning() << "no nothing";
+	return true;
+}
+
+bool GScreenSaver::doClose() {
+	qWarning() << "no nothing";
+	return true;
+}
+#endif // QT_GUI_LIB
---
+++ src/net/process/gscreensaver.h
@@ -0,0 +1,83 @@
+// ----------------------------------------------------------------------------
+//
+// G Library
+//
+// http://gilgil.net
+//
+// Copyright (c) Gilbert Lee All rights reserved
+//
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include "base/gstateobj.h"
+
+#ifdef QT_GUI_LIB
+#include <QWidget>
+#include <QDateTime>
+#include <QLabel>
+#include <QTimer>
+
+// ----------------------------------------------------------------------------
+// ScreenSaverWidget
+// ----------------------------------------------------------------------------
+struct GScreenSaver;
+struct ScreenSaverWidget : QWidget {
+	Q_OBJECT
+
+public:
+	GScreenSaver* screenSaver_;
+	QLabel* label_{nullptr};
+	QTimer* timer_{nullptr};
+
+public:
+	explicit ScreenSaverWidget(GScreenSaver* screenSaver);
+	~ScreenSaverWidget() override;
+
+protected:
+	void closeEvent(QCloseEvent *event) override;
+	void mousePressEvent(QMouseEvent *event) override;
+
+public slots:
+	void updateCurrentTime();
+};
+
+// ----------------------------------------------------------------------------
+// GScreenSaver
+// ----------------------------------------------------------------------------
+struct G_EXPORT GScreenSaver : GStateObj {
+	Q_OBJECT
+	Q_PROPERTY(QString format MEMBER format_)
+	Q_PROPERTY(int updateInterval MEMBER updateInterval_)
+	Q_PROPERTY(ShowStyle showStyle MEMBER showStyle_)
+	Q_ENUMS(ShowStyle)
+
+public:
+	enum ShowStyle {
+		Show,
+		ShowMaxized,
+		ShowFullScreen
+	};
+
+public:
+	QString format_{"h:mm AP"};
+	int updateInterval_{10000}; // 10 sec
+	ShowStyle showStyle_{ShowFullScreen};
+
+public:
+	Q_INVOKABLE GScreenSaver(QObject* parent = nullptr) : GStateObj(parent) {}
+	~GScreenSaver() override { close(); }
+
+protected:
+	ScreenSaverWidget* screenSaverWidget_{nullptr};
+
+protected:
+	bool doOpen() override;
+	bool doClose() override;
+};
+#else // QT_GUI_LIB
+struct G_EXPORT GScreenSaver : GStateObj {
+	Q_INVOKABLE GScreenSaver(QObject* parent = nullptr) : GStateObj(parent) {}
+	~GScreenSaver() override { close(); }
+}
+#endif // QT_GUI_LIB
