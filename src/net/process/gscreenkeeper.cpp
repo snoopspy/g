a7cd3d2aa43ebd157634f861a3ab0ac96ec58f52
gilgil <gilgil@gilgil.net>
2023-11-13 22:34:57 +0900

    Fix bugs

--- src/net/process/gscreenkeeper.cpp
+++ src/net/process/gscreenkeeper.cpp
@@ -14,33 +14,39 @@ bool GScreenKeeper::doOpen() {
 }
 
 bool GScreenKeeper::doClose() {
-	return keepScreen(true);
+	return keepScreen(false);
 }
 
 bool GScreenKeeper::keepScreen(bool on) {
-	QAndroidJniObject activity = QtAndroid::androidActivity();
-	if (!activity.isValid()) {
-		SET_ERR(GErr::Fail, "activity is not valid");
-		return false;
-	}
+	bool res = true;
+	QtAndroid::runOnAndroidThread([this, on, &res] {
+		qDebug() << "";
 
-	QAndroidJniObject window = activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
-	if (!window.isValid()) {
-		SET_ERR(GErr::Fail, "window is not valid");
-		return false;
-	}
+		QAndroidJniObject activity = QtAndroid::androidActivity();
+		if (!activity.isValid()) {
+			SET_ERR(GErr::Fail, "activity is not valid");
+			res = false;
+			return;
+		}
 
-	static const int FLAG_KEEP_SCREEN_ON = 128;
-	if (on)
-		window.callMethod<void>("addFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
-	else
-		window.callMethod<void>("clearFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
+		QAndroidJniObject window = activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
+		if (!window.isValid()) {
+			SET_ERR(GErr::Fail, "window is not valid");
+			res = false;
+			return;
+		}
 
-	QAndroidJniEnvironment env;
-	if (env->ExceptionCheck()) {
-		env->ExceptionClear();
-	}
-	return true;
+		static const int FLAG_KEEP_SCREEN_ON = 128;
+		if (on)
+			window.callMethod<void>("addFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
+		else
+			window.callMethod<void>("clearFlags", "(I)V", FLAG_KEEP_SCREEN_ON);
+
+		QAndroidJniEnvironment env;
+		if (env->ExceptionCheck())
+			env->ExceptionClear();
+	});
+	return res;
 }
 #else // Q_OS_ANDROID
 bool GScreenKeeper::doOpen() {
