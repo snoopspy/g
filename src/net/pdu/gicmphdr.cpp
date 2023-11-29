061c972ca0ac6079136743638e6fac6804011a86
gilgil <gilgil@gilgil.net>
2023-11-29 23:34:29 +0900

    Add GIcmpHdr class

--- src/net/pdu/gicmphdr.cpp
+++ src/net/pdu/gicmphdr.cpp
@@ -1,1 +1,81 @@
 #include "gicmphdr.h"
+
+// ----------------------------------------------------------------------------
+// GIcmpHdr
+// ----------------------------------------------------------------------------
+uint16_t GIcmpHdr::calcChecksum(GIpHdr* ipHdr, GIcmpHdr* icmpHdr) {
+	uint32_t res = 0;
+	int icmpHdrDataLen = ipHdr->tlen() - ipHdr->hlen() * 4;
+
+	// Add udpHdr & data buffer as array of uint16_t
+	uint16_t* p = reinterpret_cast<uint16_t*>(icmpHdr);
+	for (int i = 0; i < icmpHdrDataLen / 2; i++) {
+		res += htons(*p);
+		p++;
+	}
+
+	// If length is odd, add last data(padding)
+	if (icmpHdrDataLen % 2 != 0)
+		res += uint32_t(*(reinterpret_cast<uint8_t*>(p)) << 8);
+
+	// Decrease checksum from sum
+	res -= icmpHdr->sum();
+
+	// Recalculate sum
+	if (res >> 16)
+		res = (res & 0xFFFF) + (res >> 16);
+	res = ~res;
+
+	return uint16_t(res);
+}
+
+// ----------------------------------------------------------------------------
+// GTEST
+// ----------------------------------------------------------------------------
+#ifdef GTEST
+#include <gtest/gtest.h>
+
+#include "net/capture/gsyncpcapfile.h"
+struct GIcmpHdrTest : testing::Test {
+	GSyncPcapFile pcapFile_;
+	void SetUp() override {
+		pcapFile_.fileName_ = "pcap/eth-icmp8.pcap";
+		ASSERT_TRUE(pcapFile_.open());
+	}
+	void TearDown() override {
+		ASSERT_TRUE(pcapFile_.close());
+	}
+};
+
+TEST_F(GIcmpHdrTest, allTest) {
+	int frameNumber = 1;
+	while (true) {
+		GEthPacket packet;
+		GPacket::Result res = pcapFile_.read(&packet);
+		if (res != GPacket::Ok) break;
+
+		GIpHdr* ipHdr = packet.ipHdr_;
+		EXPECT_NE(ipHdr, nullptr);
+
+		GIcmpHdr* icmpHdr = packet.icmpHdr_;
+		EXPECT_NE(icmpHdr, nullptr);
+
+		//
+		// field test
+		//
+		if (frameNumber++ % 2 == 1)
+			EXPECT_EQ(icmpHdr->type(), GIcmpHdr::PingRequest);
+		else
+			EXPECT_EQ(icmpHdr->type(), GIcmpHdr::PingReply);
+		EXPECT_EQ(icmpHdr->code(), 0);
+
+		//
+		// checksum test
+		//
+		uint16_t realSum = icmpHdr->sum();
+		uint16_t calcSum = GIcmpHdr::calcChecksum(ipHdr, icmpHdr);
+		EXPECT_EQ(realSum, calcSum);
+	}
+}
+
+#endif // GTEST
--- src/net/pdu/gicmphdr.h
+++ src/net/pdu/gicmphdr.h
@@ -10,13 +10,30 @@
 
 #pragma once
 
-#include "gpdu.h"
+#include "giphdr.h"
 
 // ----------------------------------------------------------------------------
 // GIcmpHdr
 // ----------------------------------------------------------------------------
 #pragma pack(push, 1)
 struct G_EXPORT GIcmpHdr final {
+	uint8_t type_;
+	uint8_t code_;
+	uint16_t sum_;
+
+	uint8_t type() { return type_; }
+	uint8_t code() { return code_; }
+	uint16_t sum() { return ntohs(sum_); }
+
+	// Type(type_)
+	enum: uint8_t {
+		PingReply = 0,
+		DestinationUnReaclable = 3,
+		PingRequest = 8,
+		TtlExceeded = 11
+	};
+
+	static uint16_t calcChecksum(GIpHdr* ipHdr, GIcmpHdr* icmpHdr);
 };
 typedef GIcmpHdr *PIcmpHdr;
 #pragma pack(pop)
