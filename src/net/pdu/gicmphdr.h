59b4e7efa6b8c43445554d508762538f93ab3e73
gilgil <gilgil@gilgil.net>
2023-12-04 23:09:16 +0900

    Add GIcmpPingHdr class

--- src/net/pdu/gicmphdr.h
+++ src/net/pdu/gicmphdr.h
@@ -16,7 +16,7 @@
 // GIcmpHdr
 // ----------------------------------------------------------------------------
 #pragma pack(push, 1)
-struct G_EXPORT GIcmpHdr final {
+struct G_EXPORT GIcmpHdr {
 	uint8_t type_;
 	uint8_t code_;
 	uint16_t sum_;
@@ -36,4 +36,30 @@ struct G_EXPORT GIcmpHdr final {
 	static uint16_t calcChecksum(GIpHdr* ipHdr, GIcmpHdr* icmpHdr);
 };
 typedef GIcmpHdr *PIcmpHdr;
+
+// PingRequest or PingReply
+struct G_EXPORT GIcmpPingHdr : GIcmpHdr {
+	uint16_t id_;
+	uint16_t seq_;
+	struct timeval ts_;
+	uint32_t unused_;
+
+	uint16_t id() { return ntohs(id_); }
+	uint16_t seq() { return ntohs(id_); }
+	struct timeval ts() {
+		struct timeval res;
+		res.tv_sec = ntohl(ts_.tv_sec);
+		res.tv_usec = ntohl(ts_.tv_usec);
+		return res;
+	}
+	void* data() { return pbyte(this) + sizeof(GIcmpPingHdr); }
+};
+typedef GIcmpPingHdr *PIcmpPingHdr;
+
+struct G_EXPORT GIcmpTtlExceededHdr : GIcmpHdr {
+	uint32_t unused_;
+	GIpHdr* ipHdr() { return PIpHdr(pbyte(this) + sizeof(GIcmpTtlExceededHdr)); }
+};
+typedef GIcmpTtlExceededHdr *PIcmpTtlExceededHdr;
+
 #pragma pack(pop)
