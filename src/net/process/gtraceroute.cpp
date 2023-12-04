c079da7aa10eb1d15d2197ea2b9b8a82209edc79
gilgil <gilgil@gilgil.net>
2023-12-05 01:23:28 +0900

    Change a lot

--- src/net/process/gtraceroute.cpp
+++ src/net/process/gtraceroute.cpp
@@ -12,13 +12,6 @@ GTraceRoute::~GTraceRoute() {
 }
 
 bool GTraceRoute::doOpen() {
-	if (ipMgr_ == nullptr) {
-		SET_ERR(GErr::ObjectIsNull, "ipMgr must be specified");
-		return false;
-	}
-	ipOffset_ = ipMgr_->requestItems_.request(this, sizeof(Item));
-	ipMgr_->managables_.insert(this);
-
 	if (pcapDevice_ == nullptr) {
 		SET_ERR(GErr::ObjectIsNull, "pcapDevice is null");
 		return false;
@@ -27,6 +20,19 @@ bool GTraceRoute::doOpen() {
 	intf_ = pcapDevice_->intf();
 	if (intf_ == nullptr) {
 		SET_ERR(GErr::ObjectIsNull, "intf is null");
+		return false;
+	}
+	myIp_ = intf_->ip();
+
+	if (ipMgr_ == nullptr) {
+		SET_ERR(GErr::ObjectIsNull, "ipMgr is null");
+		return false;
+	}
+	ipOffset_ = ipMgr_->requestItems_.request(this, sizeof(Item));
+	ipMgr_->managables_.insert(this);
+
+	if (rawIpSocketWrite_ == nullptr) {
+		SET_ERR(GErr::ObjectIsNull, "rawIpSocketWrite is null");
 		return false;
 	}
 
@@ -49,7 +55,7 @@ bool GTraceRoute::doClose() {
 		{
 			QMutexLocker ml(&testThreadMap_);
 			int count = testThreadMap_.count();
-			qDebug() << QString("testThreadSet count=%1").arg(count);
+			qDebug() << QString("testThreadMap_.count()=%1").arg(count);
 			if (count == 0) break;
 		}
 		QCoreApplication::processEvents();
@@ -57,7 +63,7 @@ bool GTraceRoute::doClose() {
 		quint64 now = timer.elapsed();
 		if (now - start > G::Timeout) {
 			int count = testThreadMap_.count();
-			qCritical() << QString("floodingThreadSet_.count() is %1").arg(count);
+			qCritical() << QString("testThreadMap_.count()=%1").arg(count);
 			break;
 		}
 	}
@@ -65,56 +71,126 @@ bool GTraceRoute::doClose() {
 }
 
 void GTraceRoute::ipCreated(GIp ip, GIpMgr::IpValue* ipValue) {
+	qDebug() << ""; // gilgil temp 2023.12.05
 	(void)ip;
 	Item* item = PItem(ipValue->mem(ipOffset_));
 	new (item) Item;
-	qDebug() << "";
 }
 
 void GTraceRoute::ipDeleted(GIp ip, GIpMgr::IpValue* ipValue) {
+	qDebug() << ""; // gilgil temp 2023.12.05
 	(void)ip;
 	Item* item = PItem(ipValue->mem(ipOffset_));
 	item->~Item();
-	qDebug() << "";
+}
+
+void GTraceRoute::processPacketResponse(GPacket* packet, bool* ok) {
+	(void)ok;
+
+	GIpHdr* ipHdr = packet->ipHdr_;
+	if (ipHdr == nullptr) return;
+
+	GIp dip = ipHdr->dip();
+	if (dip != myIp_) return;
+	GIcmpHdr* icmpHdr = packet->icmpHdr_;
+	if (icmpHdr != nullptr) {
+		uint8_t type = icmpHdr->type();
+		switch (type) {
+			case GIcmpHdr::TtlExceeded: {
+				GIcmpIpHdr* icmpIpHdr = PIcmpIpHdr(icmpHdr);
+				GIpHdr* nextIpHdr = icmpIpHdr->ipHdr();
+				// GIp nextSip = nextIpHdr->sip();
+				// GIp nextDip = nextIpHdr->dip();
+				// {
+				// 	QMutexLocker ml(&testThreadMap_);
+				// 	TestThreadMap::iterator it = testThreadMap_.find(nextDip);
+				// 	if (it != testThreadMap_.end());
+
+				// }
+				qDebug() << QString("%1 %2 %3").arg(QString(ipHdr->sip())).arg(QString(nextIpHdr->dip())).arg(nextIpHdr->id());
+			}
+			default: break;
+		}
+	}
+}
+
+void GTraceRoute::processCreateThread(GPacket* packet) {
+	GIpHdr* ipHdr = packet->ipHdr_;
+	if (ipHdr == nullptr) return;
+
+	GIp sip = ipHdr->sip();
+	GIp dip = ipHdr->dip();
+	if (dip == myIp_) return;
+	if (dip != ipMgr_->currentIp_) return;
+	Q_ASSERT(ipMgr_->currentIpVal_ != nullptr);
+	Item* item = PItem(ipMgr_->currentIpVal_->mem(ipOffset_));
+	if (!item->active_) {
+		GFlow::IpFlowKey testFlowKey(sip, dip);
+		QMetaObject::invokeMethod(this, [this, packet, testFlowKey]() {
+			TestThread* thread = new TestThread(this, packet, testFlowKey);
+			QObject::connect(thread, &QThread::finished, thread, &QObject::deleteLater);
+			thread->start();
+		}, Qt::BlockingQueuedConnection);
+		item->active_ = true;
+	}
 }
 
 void GTraceRoute::test(GPacket* packet) {
-	(void)packet;
-	Q_ASSERT(ipMgr_->currentIpVal_ != nullptr);
-	Item* item = PItem(ipMgr_->currentIpVal_->mem(ipOffset_));
-	if (item->active_) return;
-	GIp ip = ipMgr_->currentIp_;
-	QMetaObject::invokeMethod(this, [this, packet, ip]() {
-		TestThread* thread = new TestThread(this, packet, ip);
-		QObject::connect(thread, &QThread::finished, thread, &QObject::deleteLater);
-		thread->start();
-	}, Qt::QueuedConnection);
-	item->active_ = true;
+	bool ok = false;
+	processPacketResponse(packet, &ok);
+	if (ok) return;
+	processCreateThread(packet);
 }
 
-GTraceRoute::TestThread::TestThread(GTraceRoute* tr, GPacket* packet, GIp ip) : GThread(tr) {
+GTraceRoute::TestThread::TestThread(GTraceRoute* tr, GPacket* packet, GFlow::IpFlowKey testFlowKey) : GThread(tr) {
 	GDEBUG_CTOR
-	tr_ = tr;
-	ip_ = ip;
-	packet_ = anyPacket_.get(packet->dlt());
-	packetByteArray_.resize(packet->buf_.size_);
-	GBuf buf(pbyte(packetByteArray_.data()), packetByteArray_.size());
-	packet_->copyFrom(packet, buf);
+	testFlowKey_ = testFlowKey;
+
+	sendPacket_ = anyPacket_.get(packet->dlt());
+	sendPacketByteArray_.resize(packet->buf_.size_);
+	memcpy(sendPacketByteArray_.data(), packet->buf_.data_, packet->buf_.size_);
+	GBuf buf(pbyte(sendPacketByteArray_.data()), sendPacketByteArray_.size());
+	sendPacket_->copyFrom(packet, buf);
 
 	{
-		QMutexLocker ml(&tr_->testThreadMap_);
-		tr->testThreadMap_.insert(ip, this);
+		QMutexLocker ml(&tr->testThreadMap_);
+		tr->testThreadMap_.insert(testFlowKey, this);
 	}
 }
 
 GTraceRoute::TestThread::~TestThread() {
+	GTraceRoute* tr = PTraceRoute(parent());
+	Q_ASSERT(tr != nullptr);
 	GDEBUG_DTOR
 	{
-		QMutexLocker ml(&tr_->testThreadMap_);
-		tr_->testThreadMap_.remove(ip_);
+		QMutexLocker ml(&tr->testThreadMap_);
+		tr->testThreadMap_.remove(testFlowKey_);
 	}
 }
 
 void GTraceRoute::TestThread::run() {
-	qDebug() << "run run run!!!";
+	qDebug() << "beg";
+
+	GTraceRoute* tr = PTraceRoute(parent());
+	Q_ASSERT(tr != nullptr);
+
+	if (swe_.wait(tr->startTimeout_)) return;
+	GIpHdr* ipHdr = sendPacket_->ipHdr_;
+	Q_ASSERT(ipHdr != nullptr);
+	for (int ttl = 1; ttl <= tr->maxHop_; ttl++) {
+		for (int q = 0; q < tr->queryCount_; q++) {
+			ipHdr->tos_ = 0x44;
+			ipHdr->id_ = htons(ttl);
+			ipHdr->ttl_ = ttl;
+			ipHdr->sum_ = htons(GIpHdr::calcChecksum(ipHdr));
+			tr->rawIpSocketWrite_->write(sendPacket_);
+			swe_.wait(tr->sendTimeout_);
+			if (!tr->active()) break;
+		}
+		if (!tr->active()) break;
+		swe_.wait(tr->ttlChangeTimeout_);
+	}
+	if (tr->active()) swe_.wait(tr->stopTimeout_);
+
+	qDebug() << "beg";
 }
--- src/net/process/gtraceroute.h
+++ src/net/process/gtraceroute.h
@@ -12,6 +12,7 @@
 
 #include "net/manage/gipmgr.h"
 #include "net/capture/gpcapdevice.h"
+#include "net/write/grawipsocketwrite.h"
 #include "base/sys/gwaitevent.h"
 
 // ----------------------------------------------------------------------------
@@ -19,17 +20,33 @@
 // ----------------------------------------------------------------------------
 struct G_EXPORT GTraceRoute : GStateObj, GIpMgr::Managable {
 	Q_OBJECT
-	Q_PROPERTY(GObjPtr ipMgr READ getIpMgr WRITE setIpMgr)
+	Q_PROPERTY(int maxHop MEMBER maxHop_)
+	Q_PROPERTY(int queryCount MEMBER queryCount_)
+	Q_PROPERTY(ulong startTimeout MEMBER startTimeout_)
+	Q_PROPERTY(ulong stopTimeout MEMBER stopTimeout_)
+	Q_PROPERTY(ulong ttlChangeTimeout MEMBER ttlChangeTimeout_)
+	Q_PROPERTY(ulong sendTimeout MEMBER sendTimeout_)
 	Q_PROPERTY(GObjPtr pcapDevice READ getPcapDevice WRITE setPcapDevice)
+	Q_PROPERTY(GObjPtr ipMgr READ getIpMgr WRITE setIpMgr)
+	Q_PROPERTY(GObjPtr rawIpSockerWrite READ getRawIpSockerWrite WRITE setRawIpSockerWrite )
 
-	GObjPtr getIpMgr() { return ipMgr_; }
-	void setIpMgr(GObjPtr value) { ipMgr_ = dynamic_cast<GIpMgr*>(value.data()); }
 	GObjPtr getPcapDevice() { return pcapDevice_; }
 	void setPcapDevice(GObjPtr value) { pcapDevice_ = dynamic_cast<GPcapDevice*>(value.data()); }
+	GObjPtr getIpMgr() { return ipMgr_; }
+	void setIpMgr(GObjPtr value) { ipMgr_ = dynamic_cast<GIpMgr*>(value.data()); }
+	GObjPtr getRawIpSockerWrite() { return rawIpSocketWrite_; }
+	void setRawIpSockerWrite(GObjPtr value) { rawIpSocketWrite_ = dynamic_cast<GRawIpSocketWrite*>(value.data()); }
 
 public:
-	GIpMgr* ipMgr_{nullptr};
+	int maxHop_{30};
+	int queryCount_{3};
+	GDuration startTimeout_{5000};
+	GDuration stopTimeout_{5000};
+	GDuration ttlChangeTimeout_{1};
+	GDuration sendTimeout_{1};
 	GPcapDevice* pcapDevice_{nullptr};
+	GIpMgr* ipMgr_{nullptr};
+	GRawIpSocketWrite* rawIpSocketWrite_{nullptr};
 
 public:
 	Q_INVOKABLE GTraceRoute(QObject* parent = nullptr);
@@ -48,31 +65,44 @@ public:
 	void ipCreated(GIp ip, GIpMgr::IpValue* ipValue) override;
 	void ipDeleted(GIp ip, GIpMgr::IpValue* ipValue) override;
 
-	// --------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 	// Item
-	// --------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 	struct Item {
 		bool active_{false};
 	};
 	typedef Item *PItem;
-	// --------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
+
+protected:
+	void processPacketResponse(GPacket* packet, bool* ok);
+	void processCreateThread(GPacket* packet);
 
 public slots:
 	void test(GPacket* packet);
 
 protected:
+	// ------------------------------------------------------------------------
+	// HopMap
+	// ------------------------------------------------------------------------
+	struct HopMap : QMap<int /*hopNo*/, GIp> {
+	};
+
+	// ------------------------------------------------------------------------
+	// TestThread
+	// ------------------------------------------------------------------------
 	struct TestThread : GThread {
-		TestThread(GTraceRoute* tr, GPacket* packet, GIp ip);
+		TestThread(GTraceRoute* tr, GPacket* packet, GFlow::IpFlowKey testFlowKey);
 		~TestThread() override;
 		void run() override;
 
-		GTraceRoute* tr_;
-		GIp ip_;
+		GFlow::IpFlowKey testFlowKey_;
 		GAnyPacket anyPacket_;
-		GPacket* packet_;
-		QByteArray packetByteArray_;
+		GPacket* sendPacket_;
+		QByteArray sendPacketByteArray_;
 		GStateWaitEvent swe_;
 	};
-	struct TestThreadMap : QMap<GIp, TestThread*>, QRecursiveMutex {
+	struct TestThreadMap : QMap<GFlow::IpFlowKey, TestThread*>, QRecursiveMutex {
 	} testThreadMap_;
 };
+typedef GTraceRoute *PTraceRoute;
