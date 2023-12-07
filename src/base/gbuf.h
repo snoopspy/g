7f7b3101de994e85edf3748bc9be3103cf278807
gilgil <gilgil@gilgil.net>
2023-12-07 18:25:19 +0900

    Add contains functions

--- src/base/gbuf.h
+++ src/base/gbuf.h
@@ -15,6 +15,7 @@
 // ----------------------------------------------------------------------------
 // GBuf
 // ----------------------------------------------------------------------------
+struct QString;
 struct G_EXPORT GBuf final {
 	gbyte* data_;
 	size_t size_;
@@ -30,4 +31,18 @@ struct G_EXPORT GBuf final {
 	bool valid() {
 		return data_ != nullptr;
 	}
+
+	bool contains(void* p) {
+		if (p < data_) return false;
+		if (p >= data_ + size_) return false;
+		return true;
+	}
+
+	bool contains(GBuf o) {
+		if (o.data_ < data_) return false;
+		if (o.data_ + o.size_ > data_ + size_) return false;
+		return true;
+	}
+
+	explicit operator QString() const;
 };
