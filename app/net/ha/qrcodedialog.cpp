0c129f735d25d47b45190c3531b445abbbd8737c
gilgil <gilgil@gilgil.net>
2024-01-08 01:56:10 +0900

    Add QR Code files

---
+++ app/net/ha/qrcodedialog.cpp
@@ -0,0 +1,47 @@
+#include "qrcodedialog.h"
+
+#include <GJson>
+
+QrCodeDialog::QrCodeDialog(QWidget* parent) : QDialog(parent) {
+	resize(QSize(640, 480));
+	setWindowTitle("QR Code");
+
+	lblQrCode_ = new QLabel(this);
+	pbGenerate_ = new QPushButton(this);
+	tcpServer_ = new QTcpServer(this);
+	generator_ = new QrCodeGenerator(this);
+
+	pbGenerate_->setText("Generate QR Code");
+	if (!tcpServer_->listen(QHostAddress("127.0.0.1"), port_)) {
+		QMessageBox mb;
+		mb.warning(this, "Error", QString("Can not open port %1").arg(port_));
+	}
+
+	QVBoxLayout* vLayout = new QVBoxLayout(this);
+	vLayout->addWidget(lblQrCode_);
+	vLayout->addWidget(pbGenerate_);
+	this->setLayout(vLayout);
+
+	QObject::connect(pbGenerate_, &QPushButton::clicked, this, &QrCodeDialog::pbGenerate_clicked);
+}
+
+QrCodeDialog::~QrCodeDialog() {
+	tcpServer_->close();
+}
+
+void QrCodeDialog::pbGenerate_clicked(bool checked) {
+	(void)checked;
+
+	QString text = "10.1.1.3/12345678";
+	int size = std::min(lblQrCode_->width(), lblQrCode_->height());
+	QImage image = generator_->generateQr(text, size);
+	lblQrCode_->setPixmap(QPixmap::fromImage(image));
+}
+
+void QrCodeDialog::propLoad(QJsonObject jo) {
+	jo["rect"] >> GJson::rect(this);
+}
+
+void QrCodeDialog::propSave(QJsonObject& jo) {
+	jo["rect"] << GJson::rect(this);
+}
---
+++ app/net/ha/qrcodedialog.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QDialog>
+#include <QImage>
+#include <QLabel>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QTcpServer>
+#include <QVBoxLayout>
+
+#include <GProp>
+
+#include "QrCodeGenerator.h"
+
+struct QrCodeDialog : QDialog, GProp {
+	Q_OBJECT
+	Q_PROPERTY(int port MEMBER port_)
+
+public:
+	int port_{1234};
+
+public:
+	QrCodeDialog(QWidget* parent);
+	~QrCodeDialog() override;
+
+	QLabel* lblQrCode_;
+	QPushButton* pbGenerate_;
+	QTcpServer* tcpServer_;
+	QrCodeGenerator* generator_;
+
+public slots:
+	void pbGenerate_clicked(bool checked = false);
+
+public:
+	void propLoad(QJsonObject jo) override;
+	void propSave(QJsonObject& jo) override;
+};
