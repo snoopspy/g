f07331b04f72a791ee08d7d18d3c3c13b3b60877
gilgil <gilgil@gilgil.net>
2024-02-13 00:47:55 +0900

    Add blankString

--- src/base/prop/gpropitem-checkbox.cpp
+++ src/base/prop/gpropitem-checkbox.cpp
@@ -7,6 +7,10 @@
 // ----------------------------------------------------------------------------
 GPropItemCheckBox::GPropItemCheckBox(GPropItemParam* param) : GPropItem(param) {
 	checkBox_ = new GCheckBox(param->treeWidget_);
+	QString blankString;
+	for (int i = 0; i < 1024; i++)
+		blankString += ' ';
+	checkBox_->setText(blankString);
 	param->treeWidget_->setItemWidget(item_, 1, checkBox_);
 }
 
