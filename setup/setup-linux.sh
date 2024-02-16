bb092616b30c8f1a83fc105f2b2e5bdba453787d
gilgil <gilgil@gilgil.net>
2024-02-16 16:20:30 +0900

    Add apt install items

--- setup/setup-linux.sh
+++ setup/setup-linux.sh
@@ -1,5 +1,15 @@
 # libpcap-dev
 sudo apt install libpcap-dev
 
+# libssl-dev
+sudo apt install libssl-dev
+
 # libnetfilter-queue
 sudo apt install libnetfilter-queue-dev
+
+# opengl
+sudo apt install libglu1-mesa-dev
+
+# xcb platform
+sudo apt install libxcb-cursor0
+
