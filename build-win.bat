3e67fda4a55bd4d829c7e520df86a81581011936
gilgil <gilgil@gilgil.net>
2024-02-16 01:25:35 -0800

    Update windows build batch file for Qt6.5.3

--- build-win.bat
+++ build-win.bat
@@ -47,7 +47,6 @@ copy ..\..\bin\arprecover.exe . & strip arprecover.exe
 copy ..\..\bin\ha.exe . & strip ha.exe
 copy ..\..\bin\netclient.exe . & strip netclient.exe
 copy ..\..\bin\netserver.exe . & strip netserver.exe
-copy ..\..\bin\pa.exe . & strip pa.exe
 copy ..\..\bin\snoopspy.exe . & strip snoopspy.exe
 copy ..\..\bin\sscon.exe . & strip sscon.exe
 copy ..\..\bin\sslog.exe . & strip sslog.exe
@@ -58,26 +57,26 @@ mkdir ss
 copy ..\..\bin\ss\* ss\
 
 rem
-rem mingw files
-rem
-set MINGW_DIR=C:\Qt\5.15.2\mingw81_64\bin
-copy %MINGW_DIR%\libgcc_s_seh-1.dll .
-copy %MINGW_DIR%\"libstdc++-6.dll" .
-copy %MINGW_DIR%\libwinpthread-1.dll .
-
-rem
 rem qt files
 rem
-set QT_DIR=C:\Qt\5.15.2\mingw81_64
-copy %QT_DIR%\bin\Qt5Core.dll .
-copy %QT_DIR%\bin\Qt5Gui.dll .
-copy %QT_DIR%\bin\Qt5Network.dll .
-copy %QT_DIR%\bin\Qt5Sql.dll .
-copy %QT_DIR%\bin\Qt5Widgets.dll .
+set QTDIR=C:\Qt\6.5.3\mingw_64
+copy %QTDIR%\bin\Qt6Core.dll .
+copy %QTDIR%\bin\Qt6Gui.dll .
+copy %QTDIR%\bin\Qt6Network.dll .
+copy %QTDIR%\bin\Qt6Sql.dll .
+copy %QTDIR%\bin\Qt6Widgets.dll .
 mkdir platforms
-copy %QT_DIR%\plugins\platforms\*.dll platforms\
+copy %QTDIR%\plugins\platforms\*.dll platforms\
 mkdir plugins\sqldrivers
-copy %QT_DIR%\plugins\sqldrivers\qsqlite.dll plugins\sqldrivers\
+copy %QTDIR%\plugins\sqldrivers\qsqlite.dll plugins\sqldrivers\
+
+rem
+rem mingw files
+rem
+set MINGW_DIR=C:\Qt\6.5.3\mingw_64\bin
+copy %QTDIR%\bin\libgcc_s_seh-1.dll .
+copy %QTDIR%\bin\"libstdc++-6.dll" .
+copy %QTDIR%\bin\libwinpthread-1.dll .
 
 rem
 rem compress
