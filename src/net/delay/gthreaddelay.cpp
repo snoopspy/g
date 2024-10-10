28c40bea4fc679616aab36dbedce07a2f155bd4d
gilgil <gilgil@gilgil.net>
2024-10-10 22:15:48 +0900

    Update GThreadDelay

--- src/net/delay/gthreaddelay.cpp
+++ src/net/delay/gthreaddelay.cpp
@@ -9,11 +9,22 @@
 // ----------------------------------------------------------------------------
 bool GThreadDelay::doOpen() {
 	rg_.seed();
+	bufferThread_.priority_ = QThread::TimeCriticalPriority;
 	bufferThread_.start();
 	return true;
 }
 
 bool GThreadDelay::doClose() {
+	if (flushOnClose_) {
+		QMutexLocker ml(&bufferThread_);
+		BufferThread::BufferMap* map = &bufferThread_.map_;
+		for (BufferThread::BufferMap::iterator it = map->begin(); it != map->end();) {
+			GPacket* packet = it.value();
+			emit delayed(packet);
+			free(packet->buf_.data_);
+			it = map->erase(it);
+		}
+	}
 	bufferThread_.swe_.wakeAll();
 	bufferThread_.quit();
 	bufferThread_.wait();
@@ -21,6 +32,7 @@ bool GThreadDelay::doClose() {
 }
 
 void GThreadDelay::BufferThread::run() {
+	qDebug() << priority();
 	while (threadDelay_->active()) {
 		lock();
 		ssize_t mapSize = map_.size();
@@ -31,29 +43,30 @@ void GThreadDelay::BufferThread::run() {
 		} else {
 			GDuration timeout = ULONG_MAX;
 			qint64 now = QDateTime::currentDateTime().toMSecsSinceEpoch();
+			int emitCount = 0;
 			for (BufferMap::iterator it = map_.begin(); it != map_.end();) {
 				qint64 ts = it.key();
-				if (ts > now) {
+				if (now < ts) {
 					timeout = ts - now;
 					break;
 				}
 
 				GPacket* packet = it.value();
-				qDebug() << "emit"; // gilgil temp 2024.10.07
 				emit threadDelay_->delayed(packet);
+				emitCount++;
 				free(packet->buf_.data_);
 				delete packet;
 				it = map_.erase(it);
 			}
 			unlock();
-			qDebug() << "timeout =" << timeout;
+			qDebug() << "emit =" << emitCount << " timeout =" << timeout;
 			swe_.wait(timeout);
 		}
 	}
 }
 
+static qint64 lastTs = 0; // gilgil temp 2024.10.08
 void GThreadDelay::delay(GPacket* packet) {
-	qDebug() << ""; // gilgil temp 2024.10.07
 	GPacket::Dlt dlt = packet->dlt();
 	GPacket* newPacket;
 	switch (dlt) {
@@ -73,6 +86,7 @@ void GThreadDelay::delay(GPacket* packet) {
 	qint64 ts = packet->ts_.tv_sec;
 	ts = ts * 1000 + packet->ts_.tv_usec / 1000;
 	ts += timeout_ + jitter_ * (rg_.generate() % 1024) / 1024;
+	qDebug() << "ts - lastTs =" << ts - lastTs; lastTs = ts; // gilgil temp 2024.10.08
 
 	bool wake = false;
 	{
@@ -80,15 +94,16 @@ void GThreadDelay::delay(GPacket* packet) {
 		BufferThread::BufferMap* map = &bufferThread_.map_;
 		if (map->size() == 0) wake = true;
 		else {
-			qint64 now = QDateTime::currentDateTime().toMSecsSinceEpoch();
 			BufferThread::BufferMap::iterator it = map->begin();
-			qint64 ts = it.key();
-			if (ts <= now)
+			qint64 firstTs = it.key();
+			qDebug() << "diff =" << ts - firstTs; // gilgil temp 2024.10.08
+			if (ts < firstTs) {
+				qDebug() << "first wake!!!";
 				wake = true;
+			}
 		}
 		bufferThread_.map_.insert(ts, newPacket);
 	}
-	qDebug() << "wake=" << wake;
 	if (wake)
 		bufferThread_.swe_.wakeAll();
 }
